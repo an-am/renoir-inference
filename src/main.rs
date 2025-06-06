04deb9ef1185f78f17e5fd3c9d1ab032f298dbf6
an-am <antonello.amore@icloud.com>
2025-06-06 10:24:05 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use std::sync::mpsc;
 use renoir::prelude::*;
 mod model;
 use burn::tensor::*;
-use sqlx::{postgres::{PgListener, PgNotification, PgPool}, Executor, FromRow, Pool, Postgres, Row};
+use sqlx::{postgres::{PgListener, PgPool, PgRow}, FromRow, Pool, Postgres, Row};
 use burn_ndarray::{NdArray, NdArrayDevice};
+use renoir::operator::source::ChannelSource;
 use model::deep_model::Model;
 use serde::{Serialize, Deserialize};
 use serde_json::Value;
 
-use std::fs::File;
+use std::{fs::File, time::Duration};
 use std::io::{self, Write};
 
 const MAX_REQUEST: i32 = 5;
@@ -40,52 +40,74 @@ struct Product {
     financial_status: f32
 }
 
-async fn update_needs_get_products(prediction: f32, product: Product, conn: Pool<Postgres>) -> f32 {
+async fn update_needs_get_products(prediction: f32, product: Product, pool: &PgPool) -> (i32, f32) {
     // Connect to the DB
-    println!("Model output for id {}: {}", product.row_id, prediction);
+    // println!("Model output for id {}: {}", product.row_id, prediction);
 
     //  &[&prediction, &product.financial_status, &product.row_id]
     //let mut conn = PgClient::connect(database_url, NoTls).unwrap();
-    let query = format!(
-        "UPDATE needs
-        SET risk_propensity = {}, financial_status = {}
-        WHERE id = {}", prediction, product.financial_status, product.row_id);
-    conn.execute(query.as_str()).await.unwrap();
-    
+    let _ = sqlx::query(r#"
+        UPDATE needs
+        SET risk_propensity = $1, financial_status = $2
+        WHERE id = $3
+        "#)
+        .bind(prediction)
+        .bind(product.financial_status)
+        .bind(product.row_id)
+        .execute(pool)
+        .await;
+
+    println!("Querying UPDATE {}", product.row_id);
+   
     // Retrieve products from product table
-    let query = format!(
-        "SELECT id_product, description 
-        FROM products
-        WHERE   
-            (
-                income = {}
-                OR accumulation = {}
-            )
-            AND risk <= {}", product.income_investment, product.accumulation_investment, prediction);
-    let products = conn.fetch_all(query.as_str()).await.unwrap();
+    let products: Vec<PgRow> = sqlx::query(
+        r#"
+        SELECT id_product, description 
+            FROM products
+            WHERE   
+                (
+                    income = $1
+                    OR accumulation = $2
+                )
+                AND risk <= $3
+        "#
+        )
+        .bind(product.income_investment)
+        .bind(product.accumulation_investment)
+        .bind(prediction)
+        .fetch_all(pool)
+        .await
+        .unwrap();
     
     println!("Advised {} products for id={}", products.len(), product.row_id);
 
-    for p in products {
+    /* for p in products {
         let id_product: i32 = p.get("id_product");
         let description: &str = p.get("description");
 
         println!("ID: {}, description: {}", id_product, description);
-    }
-    prediction
+    } */
+    (product.row_id, prediction)
 }
 
-async fn get_client(id: i32, conn: Pool<Postgres>) -> Client {
+async fn get_client(id: i32, pool: &PgPool) -> Client {
     // Retrieve the row through its id
-    let query = format!(
-        "SELECT id, age, gender, family_members, financial_education, 
-            income, wealth, income_investment, accumulation_investment, client_id
-        FROM needs 
-        WHERE id = {}", id);
-    let row = conn.fetch_one(query.as_str()).await.unwrap();
+    let row: PgRow = sqlx::query(
+        r#"
+        SELECT * 
+        FROM needs
+        WHERE id = $1
+        "#
+        )
+        .bind(id)
+        .fetch_one(pool)
+        .await
+        .unwrap();
+
+    println!("Querying DB {}", id);
     
     // Store the result into Client
-    Client { 
+    let client = Client { 
         row_id: row.get("id"), 
         age: row.get("age"), 
         gender: row.get("gender"), 
@@ -96,10 +118,13 @@ async fn get_client(id: i32, conn: Pool<Postgres>) -> Client {
         income_investment: row.get("income_investment"), 
         accumulation_investment: row.get("accumulation_investment"), 
         client_id: row.get("client_id") 
-    }
+    };
+    client
 }
 
 fn preprocessing(client: Client) -> (Product, Vec<f32>) {
+
+    println!("Preprocessing client {:?}", client.row_id);
 
     // Cast the data into floats and store them in a vec
     let client_vec = vec![
@@ -127,6 +152,9 @@ fn preprocessing(client: Client) -> (Product, Vec<f32>) {
         .zip(SCALER_MEANS.iter().zip(SCALER_SCALES.iter()))
         .map(|(&value, (&mean, &scale))| (value - mean) / scale)
         .collect();
+    
+    println!("Preprocessed client {:?}", client.row_id);
+    
     (product, client_vec)
 }
 
@@ -134,51 +162,55 @@ fn preprocessing(client: Client) -> (Product, Vec<f32>) {
 #[tokio::main]
 async fn main() {
     let (config, _args) = RuntimeConfig::from_args();
+    let config_clone = config.clone();
     config.spawn_remote_workers();
     let ctx = StreamContext::new(config);
 
-    let database_url = "postgres://postgres@localhost:5432/postgres";
+    let db_url = "postgres://postgres@localhost:5432/postgres";
+    std::env::set_var("DATABASE_URL", db_url);
 
-    let conn = PgPool::connect(&database_url).await.unwrap();
-    let a = conn.clone();
-    let b = conn.clone();
+    let conn_url =
+        std::env::var("DATABASE_URL").expect("Env var DATABASE_URL is required for this example.");
+    let pool = PgPool::connect(&conn_url).await.unwrap();
+
+    //let conn = PgPool::connect(&database_url).await.unwrap();
+    let pool_clone = pool.clone();
 
     // Start listening channel table_insert
-    let mut listener= PgListener::connect(&database_url).await.unwrap();
+    let mut listener= PgListener::connect(&conn_url).await.unwrap();
     let _ = listener.listen("table_insert").await;  
 
     // Load the model
     let model = Model::<NdArray<f32>>::default();
 
-    // Create an asynchronous channel, returning the sender/receiver halves; the receiver provides a blocking iterator
-    let (sender, receiver) = mpsc::channel::<PgNotification>();
-    
-    // Spawn an async task to continuously receive notifications.
-    tokio::task::spawn(async move {
-        loop {
-            match listener.recv().await {
-                Ok(notification) => {
-                    // Send the notification over the channel.
-                    if sender.send(notification).is_err() {
-                        eprintln!("Receiver dropped. Exiting notification loop.");
+    // Channel source
+    let (tx_channel, source) = ChannelSource::new(10_000 as usize, Replication::One);
+
+    // Host 0 receives the notifications and adds them to the channel
+    if config_clone.host_id().unwrap() == 0 {
+        tokio::spawn(async move {
+
+            loop {
+                let n = listener.recv().await.unwrap();
+                let _ = tx_channel.send(n);
+                
+               /*  match listener.recv().await {
+                    Ok(notification) => {
+                        // Send the notification to the Renoir source; back‑pressure handled by flume
+                        let _ = tx_channel.send(notification);
+                    }
+                    Err(e) => {
+                        eprintln!("Error receiving notification: {e:?}");
                         break;
                     }
-                }
-                Err(e) => {
-                    eprintln!("Error receiving notification: {:?}", e);
-                    break;
-                }
+                } */
             }
-        }
-    });
-    
-    let notifications_iter = receiver.into_iter();
+        });
+    }
 
-    let mut prediction_vec: Vec<f32> = Vec::new();
-
-    let s = ctx.stream_iter(notifications_iter)
-        .batch_mode(BatchMode::single())
-        .map(|notification|{
+    ctx.stream(source)
+        .batch_mode(BatchMode::adaptive(10, Duration::from_millis(100)))
+        .map(|notification| {
             // Get the notification payload
             let payload = notification.payload();
 
@@ -194,32 +226,54 @@ async fn main() {
         .group_by(|&(_id, client_id)| client_id)
         .rich_map({
             let mut counter = 0;
-            move |(_key, (id ,_client_id))| {
+            move |(_key, (id , client_id))| {
                 counter += 1;
+                println!("I have client {}, counted {}", client_id, counter);
+
                 (counter, id)
         }})
         .filter(|&(_key, (counter, _id))| counter <= MAX_REQUEST)
         .drop_key()
-        .map_async(move |(_counter, id)| get_client(id, a.clone()))
+        .map_async(move |(_counter, id)| {
+            let p = pool.clone();
+            async move { 
+                let client: Client = get_client(id, &p).await;
+                println!("Retrieved client {:?}", client.row_id);
+                client
+            }
+        })
         .map(|client| preprocessing(client))
         .map(|(product, vec)| {
+            println!("Creating tensor {}", product.row_id);
             let tensor = Tensor::<Backend, 2>::from_data(
                 TensorData::new(vec.clone(), [1, vec.clone().len()]), 
                 &NdArrayDevice::default());
+            println!("Created tensor {}", product.row_id);
             (product, tensor)
         })
         .map(move |(product, tensor)| {  
-            //let model = Model::<NdArray<f32>>::default();
-            let prediction = model.forward(tensor);
+            println!("Predicting {}", product.row_id);
+            let prediction = *model
+                .forward(tensor)
+                .to_data()
+                .to_vec::<f32>()
+                .unwrap()
+                .first()
+                .unwrap();
+            println!("Predicted {}", product.row_id);
             (product, prediction)
         })
-        .map(|(product, prediction)| {
-            let prediction = *prediction.to_data().to_vec::<f32>().unwrap().first().unwrap();
-            (product, prediction)
+        .map_async(move |(product, prediction)| {
+            let p: Pool<Postgres> = pool_clone.clone();
+            async move { 
+                let id = product.row_id.clone();
+                println!("Updating client {}", id);
+                update_needs_get_products(prediction, product, &p).await;
+                println!("Updated client {}", id);
+            }
         })
-        .map_async(move |(product, prediction)| update_needs_get_products(prediction, product,b.clone()))
-        .collect_vec();
-
+        .collect_count();
+    
     ctx.execute().await;
 
 }
---
+++ zip2-2.6.1/examples/write_dir.rs
@@ -0,0 +1,157 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+use anyhow::Context;
+use clap::{Parser, ValueEnum};
+use std::io::prelude::*;
+use zip::{result::ZipError, write::SimpleFileOptions};
+
+use std::fs::File;
+use std::path::{Path, PathBuf};
+use walkdir::{DirEntry, WalkDir};
+
+#[derive(Parser)]
+#[command(about, long_about = None)]
+struct Args {
+    // Source directory
+    source: PathBuf,
+    // Destination zipfile
+    destination: PathBuf,
+    // Compression method
+    #[arg(value_enum)]
+    compression_method: CompressionMethod,
+}
+
+#[derive(Clone, ValueEnum)]
+enum CompressionMethod {
+    Stored,
+    Deflated,
+    DeflatedZlib,
+    DeflatedZlibNg,
+    Bzip2,
+    Zstd,
+}
+
+fn main() {
+    std::process::exit(real_main());
+}
+
+fn real_main() -> i32 {
+    let args = Args::parse();
+    let src_dir = &args.source;
+    let dst_file = &args.destination;
+    let method = match args.compression_method {
+        CompressionMethod::Stored => zip::CompressionMethod::Stored,
+        CompressionMethod::Deflated => {
+            #[cfg(not(feature = "deflate-flate2"))]
+            {
+                println!("The `deflate-flate2` feature is not enabled");
+                return 1;
+            }
+            #[cfg(feature = "deflate-flate2")]
+            zip::CompressionMethod::Deflated
+        }
+        CompressionMethod::DeflatedZlib => {
+            #[cfg(not(feature = "deflate-zlib"))]
+            {
+                println!("The `deflate-zlib` feature is not enabled");
+                return 1;
+            }
+            #[cfg(feature = "deflate-zlib")]
+            zip::CompressionMethod::Deflated
+        }
+        CompressionMethod::DeflatedZlibNg => {
+            #[cfg(not(feature = "deflate-zlib-ng"))]
+            {
+                println!("The `deflate-zlib-ng` feature is not enabled");
+                return 1;
+            }
+            #[cfg(feature = "deflate-zlib-ng")]
+            zip::CompressionMethod::Deflated
+        }
+        CompressionMethod::Bzip2 => {
+            #[cfg(not(feature = "bzip2"))]
+            {
+                println!("The `bzip2` feature is not enabled");
+                return 1;
+            }
+            #[cfg(feature = "bzip2")]
+            zip::CompressionMethod::Bzip2
+        }
+        CompressionMethod::Zstd => {
+            #[cfg(not(feature = "zstd"))]
+            {
+                println!("The `zstd` feature is not enabled");
+                return 1;
+            }
+            #[cfg(feature = "zstd")]
+            zip::CompressionMethod::Zstd
+        }
+    };
+    match doit(src_dir, dst_file, method) {
+        Ok(_) => println!("done: {:?} written to {:?}", src_dir, dst_file),
+        Err(e) => eprintln!("Error: {e:?}"),
+    }
+
+    0
+}
+
+fn zip_dir<T>(
+    it: &mut dyn Iterator<Item = DirEntry>,
+    prefix: &Path,
+    writer: T,
+    method: zip::CompressionMethod,
+) -> anyhow::Result<()>
+where
+    T: Write + Seek,
+{
+    let mut zip = zip::ZipWriter::new(writer);
+    let options = SimpleFileOptions::default()
+        .compression_method(method)
+        .unix_permissions(0o755);
+
+    let prefix = Path::new(prefix);
+    let mut buffer = Vec::new();
+    for entry in it {
+        let path = entry.path();
+        let name = path.strip_prefix(prefix).unwrap();
+        let path_as_string = name
+            .to_str()
+            .map(str::to_owned)
+            .with_context(|| format!("{name:?} Is a Non UTF-8 Path"))?;
+
+        // Write file or directory explicitly
+        // Some unzip tools unzip files with directory paths correctly, some do not!
+        if path.is_file() {
+            println!("adding file {path:?} as {name:?} ...");
+            zip.start_file(path_as_string, options)?;
+            let mut f = File::open(path)?;
+
+            f.read_to_end(&mut buffer)?;
+            zip.write_all(&buffer)?;
+            buffer.clear();
+        } else if !name.as_os_str().is_empty() {
+            // Only if not root! Avoids path spec / warning
+            // and mapname conversion failed error on unzip
+            println!("adding dir {path_as_string:?} as {name:?} ...");
+            zip.add_directory(path_as_string, options)?;
+        }
+    }
+    zip.finish()?;
+    Ok(())
+}
+
+fn doit(src_dir: &Path, dst_file: &Path, method: zip::CompressionMethod) -> anyhow::Result<()> {
+    if !Path::new(src_dir).is_dir() {
+        return Err(ZipError::FileNotFound.into());
+    }
+
+    let path = Path::new(dst_file);
+    let file = File::create(path).unwrap();
+
+    let walkdir = WalkDir::new(src_dir);
+    let it = walkdir.into_iter();
+
+    zip_dir(&mut it.filter_map(|e| e.ok()), src_dir, file, method)?;
+
+    Ok(())
+}
